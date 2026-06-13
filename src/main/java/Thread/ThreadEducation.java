d237096c15908f1f223dcb0dc124c908b30f3cf4
aleksej <gerbutaa@gmail.com>
2026-06-13 12:18:34 +0500

    task-three

---
+++ src/main/java/Thread/ThreadEducation.java
@@ -0,0 +1,16 @@
+package Thread;
+
+import java.util.concurrent.*;
+
+public class ThreadEducation {
+    
+    
+    static void main(String[] args) throws InterruptedException {
+        MyThreadPool pool = new MyThreadPool(3);
+
+        for (int i = 0; i <= 10; i++) {
+            pool.execute(() -> System.out.println("Мой поток"));
+        }
+        pool.shutdown();
+    }
+}
