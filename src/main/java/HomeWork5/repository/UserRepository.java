f357f3f65ed4ef7999d0353b8eb4ccc7c13122ce
aleksej <gerbutaa@gmail.com>
2026-07-07 16:28:39 +0500

    task-5

---
+++ src/main/java/HomeWork5/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package HomeWork5.repository;
+
+import HomeWork5.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
--- src/main/resources/init-db/init.sql
+++ src/main/resources/init-db/init.sql
@@ -1,8 +1,1 @@
-\c database
-
 create schema if not exists test_schema;
-create table test_schema.users
-(
-    id          bigserial       primary key,
-    username    varchar(255)    unique
-);
